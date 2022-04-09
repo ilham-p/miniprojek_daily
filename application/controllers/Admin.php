7f633afd2d52636e23eec7889b953ab1dcad686d
Noor Alfath <alfath.noor17@gmail.com>
2022-04-09 12:32:26 +0700

    nambah logic untuk menghitung data laporan

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,12 +1,17 @@
 <?php
 
 class Admin extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
 	function index() {
 		// Dashboard
-		$this->load->view('admin/template/head');
-		$this->load->view('admin/index');
-		$this->load->view('admin/template/foot');
+		// $this->load->view('admin/template/head');
+		// $this->load->view('admin/index');
+		// $this->load->view('admin/template/foot');
+		print_r($this->Laporan->count_laporan(2,2));
 		
 	}
 }
